493a0b3f463cb9ac3b3b3355a2e1402d82bc8119
Mac Collins <mcollins@denimgroup.com>
2013-05-13 20:30:34 -0500

    Fixes the multiple scan delete message error

--- src/main/webapp/scripts/scan_page.js
+++ src/main/webapp/scripts/scan_page.js
@@ -1,7 +1,12 @@
 var addScanDeletes = function() {
-	$(".scanDelete").on("click", function() {
-		if (confirm("Are you sure you want to delete this scan and all of its results? This will also delete any WAF rules and defects associated with orphaned vulnerabilities.")) {
-			$("#" + $(this).attr("data-delete-form")).submit();
+	$(".scanDelete").each(function() {
+		if (!$(this).attr("data-has-function")) {
+			$(this).on("click", function() {
+				if (confirm("Are you sure you want to delete this scan and all of its results? This will also delete any WAF rules and defects associated with orphaned vulnerabilities.")) {
+					$("#" + $(this).attr("data-delete-form")).submit();
+				}
+			});
+			$(this).attr("data-has-function","1");
 		}
 	});
 };
