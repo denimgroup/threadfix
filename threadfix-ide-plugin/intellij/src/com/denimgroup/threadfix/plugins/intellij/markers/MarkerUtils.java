f8c012bd123dc10a34382db3406d0a9004f86cd4
Mac Collins <mcollins@denimgroup.com>
2013-12-13 13:21:21 -0600

    Adds copyrights.

--- threadfix-ide-plugin/intellij/src/com/denimgroup/threadfix/plugins/intellij/markers/MarkerUtils.java
+++ threadfix-ide-plugin/intellij/src/com/denimgroup/threadfix/plugins/intellij/markers/MarkerUtils.java
@@ -1,3 +1,26 @@
+////////////////////////////////////////////////////////////////////////
+//
+//     Copyright (c) 2009-2013 Denim Group, Ltd.
+//
+//     The contents of this file are subject to the Mozilla Public License
+//     Version 2.0 (the "License"); you may not use this file except in
+//     compliance with the License. You may obtain a copy of the License at
+//     http://www.mozilla.org/MPL/
+//
+//     Software distributed under the License is distributed on an "AS IS"
+//     basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//     License for the specific language governing rights and limitations
+//     under the License.
+//
+//     The Original Code is ThreadFix.
+//
+//     The Initial Developer of the Original Code is Denim Group, Ltd.
+//     Portions created by Denim Group, Ltd. are Copyright (C)
+//     Denim Group, Ltd. All Rights Reserved.
+//
+//     Contributor(s): Denim Group, Ltd.
+//
+////////////////////////////////////////////////////////////////////////
 package com.denimgroup.threadfix.plugins.intellij.markers;
 
 import com.denimgroup.threadfix.plugins.intellij.rest.VulnerabilityMarker;
@@ -25,13 +48,6 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * Created with IntelliJ IDEA.
- * User: mac
- * Date: 12/4/13
- * Time: 9:49 AM
- * To change this template use File | Settings | File Templates.
- */
 public class MarkerUtils {
 
     private MarkerUtils(){}
--- threadfix-ide-plugin/intellij/src/com/denimgroup/threadfix/plugins/intellij/rest/ApplicationsMap.java
+++ threadfix-ide-plugin/intellij/src/com/denimgroup/threadfix/plugins/intellij/rest/ApplicationsMap.java
@@ -1,16 +1,32 @@
+////////////////////////////////////////////////////////////////////////
+//
+//     Copyright (c) 2009-2013 Denim Group, Ltd.
+//
+//     The contents of this file are subject to the Mozilla Public License
+//     Version 2.0 (the "License"); you may not use this file except in
+//     compliance with the License. You may obtain a copy of the License at
+//     http://www.mozilla.org/MPL/
+//
+//     Software distributed under the License is distributed on an "AS IS"
+//     basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//     License for the specific language governing rights and limitations
+//     under the License.
+//
+//     The Original Code is ThreadFix.
+//
+//     The Initial Developer of the Original Code is Denim Group, Ltd.
+//     Portions created by Denim Group, Ltd. are Copyright (C)
+//     Denim Group, Ltd. All Rights Reserved.
+//
+//     Contributor(s): Denim Group, Ltd.
+//
+////////////////////////////////////////////////////////////////////////
 package com.denimgroup.threadfix.plugins.intellij.rest;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
-/**
- * Created with IntelliJ IDEA.
- * User: mac
- * Date: 12/5/13
- * Time: 1:42 PM
- * To change this template use File | Settings | File Templates.
- */
 public class ApplicationsMap {
 
     private final Map<String,Map<String, String>> map =
