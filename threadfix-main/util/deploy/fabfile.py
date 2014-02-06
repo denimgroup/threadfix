6e6c4ebdfb2c881130f77956c252822197cf37c3
mhall <mhall@denimgroup.com>
2014-02-06 15:14:27 -0600

    Updating QA deployment script for new artifact name.

--- threadfix-main/util/deploy/fabfile.py
+++ threadfix-main/util/deploy/fabfile.py
@@ -58,7 +58,7 @@ def deploy_war():
     sudo('service tomcat7 stop')   #stop tomcat
     with settings(warn_only=True):
         sudo('rm -rf %s/threadfix' % (server_base_loc))
-    sudo('mv %s/threadfix-main/target/threadfix-2.0M1-SNAPSHOT.war %s/threadfix.war' % (local_working_folder_loc, server_base_loc))
+    sudo('mv %s/threadfix-main/target/threadfix-2.0M2-SNAPSHOT.war %s/threadfix.war' % (local_working_folder_loc, server_base_loc))
     sudo('service tomcat7 start')  #start tomcat
 
 # verifies the login page
