160874ec4f64b5008cbc57987ec4e95fabd5fedb
mhall <mhall@denimgroup.com>
2014-03-03 15:47:29 -0600

    revising hsql deployment process

--- threadfix-main/util/deploy/fabfile.py
+++ threadfix-main/util/deploy/fabfile.py
@@ -10,16 +10,6 @@ env.user = 'denimgroup'
 local_working_folder_loc = os.getcwd() #where fabfile is running from
 server_base_loc = '/var/lib/tomcat7/webapps' #where to deploy to
 
-# creates the WAR file from the source code
-@task
-@runs_once
-def build_war(profile):
-    print("profile is %s" % profile)
-    with lcd('%s' % local_working_folder_loc):
-        res = local('mvn help:active-profiles clean install -DskipTests -P %s' % profile)
-    if res.failed and confirm('Maven failed to build the WAR file. Abort recommended. Abort?'):
-        abort('Aborting because Maven failed.')
-
 # moves the WAR file to the remote server, updates the database and restarts tomcat 
 @task
 @runs_once
@@ -30,11 +20,17 @@ def deploy_war():
     sudo('mv %s/threadfix-main/target/threadfix-2.0FINAL-SNAPSHOT.war %s/threadfix.war' % (local_working_folder_loc, server_base_loc))
     sudo('service tomcat7 start')  #start tomcat
 
+# moves the WAR file to the remote server, updates the database and restarts tomcat
+@task
+@runs_once
+def reset_hsql():
+    with settings(warn_only=True):
+        sudo('rm -rf /var/lib/tomcat7/database')
+    sudo('cp -r /var/lib/tomcat7/backup_database /var/lib/tomcat7/database')
+    sudo('chown tomcat7:tomcat7 /var/lib/tomcat7/database')
+
 @task(default=True)
-def deploy(profile):
-
-    env.profile = profile
-
-    build_war(profile)
+def deploy():
+    reset_hsql()
     deploy_war()
 
