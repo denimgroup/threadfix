1d4461bb0c961b17a11cce96073189eb32c4ecc7
mhall <mhall@denimgroup.com>
2014-03-13 11:02:31 -0500

    Testing Window Maximize for QA

--- threadfix-main/src/test/java/com/denimgroup/threadfix/selenium/tests/BaseTest.java
+++ threadfix-main/src/test/java/com/denimgroup/threadfix/selenium/tests/BaseTest.java
@@ -54,7 +54,8 @@ public abstract class BaseTest {
 	public void init() {
         TeamIndexCache.getCache().clear();
         loginPage = LoginPage.open(driver);
-	}
+        driver.manage().window().maximize();
+    }
 
 	@After
 	public void shutDown() {
