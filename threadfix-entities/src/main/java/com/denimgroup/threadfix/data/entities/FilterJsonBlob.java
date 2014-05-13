b969a0797b89857637d29b51bae1fc7ce03b7f48
Mac Collins <mcollins@denimgroup.com>
2014-05-13 14:43:54 -0500

    Adds first version of generic service / daos in preparation for saving filters.

---
+++ threadfix-entities/src/main/java/com/denimgroup/threadfix/data/entities/FilterJsonBlob.java
@@ -0,0 +1,55 @@
+////////////////////////////////////////////////////////////////////////
+//
+//     Copyright (c) 2009-2014 Denim Group, Ltd.
+//
+//     The contents of this file are subject to the Mozilla Public License
+//     Version 2.0 (the "License"); you may not use this file except in
+//     compliance with the License. You may obtain a copy of the License at
+//     http://www.mozilla.org/MPL/
+//
+//     Software distributed under the License is distributed on an "AS IS"
+//     basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//     License for the specific language governing rights and limitations
+//     under the License.
+//
+//     The Original Code is ThreadFix.
+//
+//     The Initial Developer of the Original Code is Denim Group, Ltd.
+//     Portions created by Denim Group, Ltd. are Copyright (C)
+//     Denim Group, Ltd. All Rights Reserved.
+//
+//     Contributor(s): Denim Group, Ltd.
+//
+////////////////////////////////////////////////////////////////////////
+package com.denimgroup.threadfix.data.entities;
+
+import org.codehaus.jackson.map.annotate.JsonView;
+import org.hibernate.annotations.Entity;
+
+import javax.persistence.Column;
+import javax.persistence.Transient;
+
+/**
+ * Created by mac on 5/13/14.
+ */
+@Entity
+public class FilterJsonBlob extends AuditableEntity{
+
+    private String json;
+
+    @JsonView(Object.class)
+    @Column(length = 102400)
+    public String getJson() {
+        return json;
+    }
+
+    public void setJson(String json) {
+        this.json = json;
+    }
+
+    @Transient
+    @Override
+    public String toString() {
+        return json;
+    }
+}
