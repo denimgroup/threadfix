fa9bdbe72da9c6ee7ca30be32a9838ba5009adcb
Mac Collins <mcollins@denimgroup.com>
2014-07-03 10:53:56 -0500

    Adds DefectTrackerUnavailableException, upgrades Spring to version 3.2.9 and switches to @ControllerAdvice instead of HandlerExceptionResolverBean

---
+++ threadfix-main/src/main/java/com/denimgroup/threadfix/exception/RestException.java
@@ -0,0 +1,48 @@
+////////////////////////////////////////////////////////////////////////
+//
+//     Copyright (c) 2009-2014 Denim Group, Ltd.
+//
+//     The contents of this file are subject to the Mozilla Public License
+//     Version 2.0 (the "License"); you may not use this file except in
+//     compliance with the License. You may obtain a copy of the License at
+//     http://www.mozilla.org/MPL/
+//
+//     Software distributed under the License is distributed on an "AS IS"
+//     basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//     License for the specific language governing rights and limitations
+//     under the License.
+//
+//     The Original Code is ThreadFix.
+//
+//     The Initial Developer of the Original Code is Denim Group, Ltd.
+//     Portions created by Denim Group, Ltd. are Copyright (C)
+//     Denim Group, Ltd. All Rights Reserved.
+//
+//     Contributor(s): Denim Group, Ltd.
+//
+////////////////////////////////////////////////////////////////////////
+package com.denimgroup.threadfix.exception;
+
+/**
+ * This is an abstract class so that we can intercept it using RestExceptionControllerAdvice
+ * Created by mac on 7/3/14.
+ */
+public abstract class RestException extends RuntimeException {
+
+    final String responseString;
+
+    public RestException(String message, Throwable cause, String responseString) {
+        super(message, cause);
+        this.responseString = responseString;
+    }
+
+    public RestException(Throwable cause, String responseString) {
+        super(cause);
+        this.responseString = responseString;
+    }
+
+    public String getResponseString() {
+        return responseString;
+    }
+
+}
