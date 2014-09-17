c99564f6a07772b4d712159ec14ae83a64e49a96
Mirko Dziadzka <mirko.dziadzka@riverbed.com>
2014-09-17 11:07:21 +0200

    reformat source code with a proper java ide
    also remove unneded imports

--- threadfix-main/src/main/java/com/denimgroup/threadfix/service/waf/RiverbedWebAppFirewallGenerator.java
+++ threadfix-main/src/main/java/com/denimgroup/threadfix/service/waf/RiverbedWebAppFirewallGenerator.java
@@ -23,12 +23,9 @@
 ////////////////////////////////////////////////////////////////////////
 package com.denimgroup.threadfix.service.waf;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import org.apache.commons.lang.StringEscapeUtils;
 
 import com.denimgroup.threadfix.data.dao.WafRuleDao;
 import com.denimgroup.threadfix.data.dao.WafRuleDirectiveDao;
@@ -46,123 +43,116 @@ import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 /**
- * @author Mirko Dziadzka @ Riverbed
- * 
+ * @author Mirko Dziadzka, Riverbed Technology
+ *
  */
-public class RiverbedWebAppFirewallGenerator extends RealTimeProtectionGenerator {	
-       public static String RULE_PROVIDER_NAME = "threadfix"; 
-       public static String RULE_PROVIDER_VERSION = "20140915";
-	
-        protected static final Map<String, String> VULNERABILITY_CLASS_MAPPING = new HashMap<String,String>(){
-            {
-                // XSS
-                put(GenericVulnerability.CWE_CROSS_SITE_SCRIPTING,      "GENERIC_CROSS_SITE_SCRIPTING");
-                // injections
-                put(GenericVulnerability.CWE_BLIND_XPATH_INJECTION,     "GENERIC_BLIND_XPATH_INJECTION");
-                put(GenericVulnerability.CWE_EVAL_INJECTION,            "GENERIC_EVAL_INJECTION");
-                put(GenericVulnerability.CWE_FORMAT_STRING_INJECTION,   "GENERIC_FORMAT_STRING_INJECTION");
-                put(GenericVulnerability.CWE_GENERIC_INJECTION,         "GENERIC_INJECTION");
-                put(GenericVulnerability.CWE_LDAP_INJECTION,            "GENERIC_LDAP_INJECTION");
-                put(GenericVulnerability.CWE_OS_COMMAND_INJECTION,      "GENERIC_COMMAND_INJECTION");
-                put(GenericVulnerability.CWE_SQL_INJECTION,             "GENERIC_SQL_INJECTION");
-                put(GenericVulnerability.CWE_XPATH_INJECTION,           "GENERIC_XPATH_INJECTION");
-                // path problems
-                put(GenericVulnerability.CWE_PATH_TRAVERSAL,            "GENERIC_PATH_TRAVERSAL");
-                put(GenericVulnerability.CWE_DIRECTORY_INDEXING,        "GENERIC_DIRECTORY_INDEXING");
-                // server (we can't handle this in the first implementation)
-                put(GenericVulnerability.CWE_HTTP_RESPONSE_SPLITTING,  "GENERIC_RESPONSE_SPLITTING");
-                put(GenericVulnerability.CWE_DIRECT_REQUEST,           "GENERIC_DIRECT_REQUEST");
-                put(GenericVulnerability.CWE_CROSS_SITE_REQUEST_FORGERY, "GENERIC_CSRF");
-                put(GenericVulnerability.CWE_FILE_UPLOAD,              "GENERIC_FILE_UPLOAD");
-                // response problems
-                put(GenericVulnerability.CWE_INFORMATION_EXPOSURE,     "GENERIC_INFORMATION_EXPOSURE");
-                put(GenericVulnerability.CWE_PRIVACY_VIOLATION,        "GENERIC_PRIVACY_VIOLATION");
-                put(GenericVulnerability.CWE_DEBUG_CODE,               "GENERIC_DEBUG_CODE");
-            }
+public class RiverbedWebAppFirewallGenerator extends RealTimeProtectionGenerator {
+
+    public static String RULE_PROVIDER_NAME = "threadfix";
+    public static String RULE_PROVIDER_VERSION = "20140915";
+    protected static final Map<String, String> VULNERABILITY_CLASS_MAPPING = new HashMap<String, String>() {
+        {
+            // XSS
+            put(GenericVulnerability.CWE_CROSS_SITE_SCRIPTING, "GENERIC_CROSS_SITE_SCRIPTING");
+            // injections
+            put(GenericVulnerability.CWE_BLIND_XPATH_INJECTION, "GENERIC_BLIND_XPATH_INJECTION");
+            put(GenericVulnerability.CWE_EVAL_INJECTION, "GENERIC_EVAL_INJECTION");
+            put(GenericVulnerability.CWE_FORMAT_STRING_INJECTION, "GENERIC_FORMAT_STRING_INJECTION");
+            put(GenericVulnerability.CWE_GENERIC_INJECTION, "GENERIC_INJECTION");
+            put(GenericVulnerability.CWE_LDAP_INJECTION, "GENERIC_LDAP_INJECTION");
+            put(GenericVulnerability.CWE_OS_COMMAND_INJECTION, "GENERIC_COMMAND_INJECTION");
+            put(GenericVulnerability.CWE_SQL_INJECTION, "GENERIC_SQL_INJECTION");
+            put(GenericVulnerability.CWE_XPATH_INJECTION, "GENERIC_XPATH_INJECTION");
+            // path problems
+            put(GenericVulnerability.CWE_PATH_TRAVERSAL, "GENERIC_PATH_TRAVERSAL");
+            put(GenericVulnerability.CWE_DIRECTORY_INDEXING, "GENERIC_DIRECTORY_INDEXING");
+            // server (we can't handle this in the first implementation)
+            put(GenericVulnerability.CWE_HTTP_RESPONSE_SPLITTING, "GENERIC_RESPONSE_SPLITTING");
+            put(GenericVulnerability.CWE_DIRECT_REQUEST, "GENERIC_DIRECT_REQUEST");
+            put(GenericVulnerability.CWE_CROSS_SITE_REQUEST_FORGERY, "GENERIC_CSRF");
+            put(GenericVulnerability.CWE_FILE_UPLOAD, "GENERIC_FILE_UPLOAD");
+            // response problems
+            put(GenericVulnerability.CWE_INFORMATION_EXPOSURE, "GENERIC_INFORMATION_EXPOSURE");
+            put(GenericVulnerability.CWE_PRIVACY_VIOLATION, "GENERIC_PRIVACY_VIOLATION");
+            put(GenericVulnerability.CWE_DEBUG_CODE, "GENERIC_DEBUG_CODE");
+        }
+    };
+
+    public RiverbedWebAppFirewallGenerator(WafRuleDao wafRuleDao, WafRuleDirectiveDao wafRuleDirectiveDao) {
+        this.wafRuleDao = wafRuleDao;
+        this.wafRuleDirectiveDao = wafRuleDirectiveDao;
+    }
+
+    @Override
+    public String[] getSupportedVulnerabilityTypes() {
+        return new String[]{
+            //xss 
+            GenericVulnerability.CWE_CROSS_SITE_SCRIPTING,
+            // injection
+            GenericVulnerability.CWE_BLIND_XPATH_INJECTION,
+            GenericVulnerability.CWE_EVAL_INJECTION,
+            GenericVulnerability.CWE_FORMAT_STRING_INJECTION,
+            GenericVulnerability.CWE_GENERIC_INJECTION,
+            GenericVulnerability.CWE_LDAP_INJECTION,
+            GenericVulnerability.CWE_OS_COMMAND_INJECTION,
+            GenericVulnerability.CWE_SQL_INJECTION,
+            GenericVulnerability.CWE_XPATH_INJECTION, 
         };
+    }
 
-	public RiverbedWebAppFirewallGenerator(WafRuleDao wafRuleDao, WafRuleDirectiveDao wafRuleDirectiveDao) {
-		this.wafRuleDao = wafRuleDao;
-		this.wafRuleDirectiveDao = wafRuleDirectiveDao;
-	}
-	
-	@Override
-	public String[] getSupportedVulnerabilityTypes() {
-		return new String[] { 
-                                    //xss 
-                                    GenericVulnerability.CWE_CROSS_SITE_SCRIPTING,
-                                    // injection
-                                    GenericVulnerability.CWE_BLIND_XPATH_INJECTION,
-                                    GenericVulnerability.CWE_EVAL_INJECTION,
-                                    GenericVulnerability.CWE_FORMAT_STRING_INJECTION,
-                                    GenericVulnerability.CWE_GENERIC_INJECTION,
-                                    GenericVulnerability.CWE_LDAP_INJECTION,
-                                    GenericVulnerability.CWE_OS_COMMAND_INJECTION,
-                                    GenericVulnerability.CWE_SQL_INJECTION,
-                                    GenericVulnerability.CWE_XPATH_INJECTION,
-                                    //path problems
-                                    //server
-                                    //response problems
-                                };
-	}
+    public static String getStart(List<WafRule> rules) {
+        return "{\"provider\":\"" + RULE_PROVIDER_NAME + "\",\"version\":\"" + RULE_PROVIDER_VERSION + "\",\"rules\":[\n";
+    }
 
-        public static String getStart(List<WafRule> rules) {
-                return "{\"provider\":\"" + RULE_PROVIDER_NAME + "\",\"version\":\""+ RULE_PROVIDER_VERSION +"\",\"rules\":[\n";
-        }
+    public static String getEnd(List<WafRule> rules) {
+        // add empty element to list of protection rules
+        return "\tnull]\n}\n";
+    }
 
-        public static String getEnd(List<WafRule> rules) {
-                // add empty element to list of protection rules
-                return "\tnull]\n}\n";
-        }
+    @Override
+    protected String generateRuleWithParameter(String uri, String action, String id,
+            String genericVulnName, String parameter) {
+        JsonObject res = new JsonObject();
 
-	
+        res.addProperty("match", "args");
+        res.addProperty("uri", uri);
+        res.addProperty("action", action);
+        res.addProperty("id", id);
+        res.addProperty("genericVulnName", VULNERABILITY_CLASS_MAPPING.get(genericVulnName));
+        res.addProperty("parameter", parameter);
 
+        return "\t" + res.toString() + ",";
+    }
 
-	@Override
-	protected String generateRuleWithParameter(String uri, String action, String id,
-			String genericVulnName, String parameter) {
-                JsonObject res = new JsonObject();
+    @Override
+    protected String generateRuleWithPayloadInUrl(String uri, String action, String id,
+            String genericVulnName) {
 
-                res.addProperty("match", "args");
-                res.addProperty("uri", uri);
-                res.addProperty("action", action);
-                res.addProperty("id", id);
-                res.addProperty("genericVulnName", VULNERABILITY_CLASS_MAPPING.get(genericVulnName));
-                res.addProperty("parameter", parameter);
+        JsonObject res = new JsonObject();
 
-                return "\t" + res.toString() + ",";
-	}
-	
-	@Override
-	protected String generateRuleWithPayloadInUrl(String uri, String action, String id,
-			String genericVulnName) {
+        res.addProperty("match", "uri");
+        res.addProperty("uri", uri);
+        res.addProperty("action", action);
+        res.addProperty("id", id);
+        res.addProperty("genericVulnName", VULNERABILITY_CLASS_MAPPING.get(genericVulnName));
 
-                JsonObject res = new JsonObject();
+        return "\t" + res.toString() + ",";
 
-                res.addProperty("match", "uri");
-                res.addProperty("uri", uri);
-                res.addProperty("action", action);
-                res.addProperty("id", id);
-                res.addProperty("genericVulnName", VULNERABILITY_CLASS_MAPPING.get(genericVulnName));
+    }
 
-                return "\t" + res.toString() + ",";
-		
-	}
-	
-	@Override
-	protected String generateRuleForExactUrl(String uri, String action, String id,
-			String genericVulnName) {
+    @Override
+    protected String generateRuleForExactUrl(String uri, String action, String id,
+            String genericVulnName) {
 
 
-                JsonObject res = new JsonObject();
+        JsonObject res = new JsonObject();
 
-                res.addProperty("match", "uri");
-                res.addProperty("uri", uri);
-                res.addProperty("action", action);
-                res.addProperty("id", id);
-                res.addProperty("genericVulnName", VULNERABILITY_CLASS_MAPPING.get(genericVulnName));
+        res.addProperty("match", "uri");
+        res.addProperty("uri", uri);
+        res.addProperty("action", action);
+        res.addProperty("id", id);
+        res.addProperty("genericVulnName", VULNERABILITY_CLASS_MAPPING.get(genericVulnName));
 
-                return "\t" + res.toString() + ",";
-	}
-	
+        return "\t" + res.toString() + ",";
+    }
 }
--- threadfix-main/src/main/java/com/denimgroup/threadfix/service/waflog/RiverbedWebAppFirewallLogParser.java
+++ threadfix-main/src/main/java/com/denimgroup/threadfix/service/waflog/RiverbedWebAppFirewallLogParser.java
@@ -23,9 +23,6 @@
 ////////////////////////////////////////////////////////////////////////
 package com.denimgroup.threadfix.service.waflog;
 
-import java.math.BigInteger;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -42,189 +39,182 @@ import com.denimgroup.threadfix.data.entities.SecurityEvent;
 import com.denimgroup.threadfix.data.entities.WafRule;
 import com.denimgroup.threadfix.logging.SanitizedLogger;
 
-
 /**
- * @author: Mirko Dziadzka @ Riverbed Technology
+ * @author: Mirko Dziadzka, Riverbed Technology
+ *
  */
-
 public class RiverbedWebAppFirewallLogParser extends WafLogParser {
 
-        protected final SanitizedLogger log = new SanitizedLogger(this.getClass());
-        private static final String THREADFIX_HANDLER_NAME = "ThreadfixHandler";
-        private static final String THREADFIX_HANDLER_COMPONENT = "protection_rules";
-        private static final String LOG_TIMESTAMP_FORMAT = "yyyyMMdd-HHmmss"; // 20140131-172342
-	private static final String WAF_LOG_MODE_PROTECTION = "P";
-	private static final String WAF_LOG_MODE_DETECTION = "D";
+    protected final SanitizedLogger log = new SanitizedLogger(this.getClass());
+    private static final String THREADFIX_HANDLER_NAME = "ThreadfixHandler";
+    private static final String THREADFIX_HANDLER_COMPONENT = "protection_rules";
+    private static final String LOG_TIMESTAMP_FORMAT = "yyyyMMdd-HHmmss"; // 20140131-172342
+    private static final String WAF_LOG_MODE_PROTECTION = "P";
+    private static final String WAF_LOG_MODE_DETECTION = "D";
 
-	/**
-	 * @param wafRuleDao
-	 * @param securityEventDao
-	 */
-	public RiverbedWebAppFirewallLogParser(WafRuleDao wafRuleDao, SecurityEventDao securityEventDao) {
-		this.wafRuleDao = wafRuleDao;
-		this.securityEventDao = securityEventDao;
-	}
+    /**
+     * @param wafRuleDao
+     * @param securityEventDao
+     */
+    public RiverbedWebAppFirewallLogParser(WafRuleDao wafRuleDao, SecurityEventDao securityEventDao) {
+        this.wafRuleDao = wafRuleDao;
+        this.securityEventDao = securityEventDao;
+    }
 
-	/**
-	 * @param entryBuffer
-	 * @return
-	 */
-	@Override
-	public SecurityEvent getSecurityEvent(String entry) {
-		if (entry == null || entry.isEmpty() || entry.startsWith("#")) 
-			return null;
+    /**
+     * @param entryBuffer
+     * @return
+     */
+    @Override
+    public SecurityEvent getSecurityEvent(String entry) {
+        if (entry == null || entry.isEmpty() || entry.startsWith("#")) {
+            return null;
+        }
 
-                // a logline is a csv encoded line with the following columns
-                //  * [0] a timestamp: YYYYMMDD-HHMMSS in local time
-                //  * [1] an internal session id or "default"
-                //  * [2] internal cluster node id
-                //  * [3] host header
-                //  * [4] client ip
-                //  * [5] HTTP method
-                //  * [6] URL
-                //  * [7] HTTP protocol version
-                //  * [8] internal ruleset / rule id
-                //  * [9] action
-                //  * [10] protection or detection mode
-                //  * [11] request or response
-                //  * [12] handlerName - we only care for the THREADFIX_HANDLER_NAME here
-                //  * [13] component which reject the request
-                //  * [14] value which rejects the request
-                //  * [16] error id (use this together with the timetamp to be unique)
-                //  * [17] free text field
-                //  * ... aditional stuff
+        // a logline is a csv encoded line with the following columns
+        //  * [0] a timestamp: YYYYMMDD-HHMMSS in local time
+        //  * [1] an internal session id or "default"
+        //  * [2] internal cluster node id
+        //  * [3] host header
+        //  * [4] client ip
+        //  * [5] HTTP method
+        //  * [6] URL
+        //  * [7] HTTP protocol version
+        //  * [8] internal ruleset / rule id
+        //  * [9] action
+        //  * [10] protection or detection mode
+        //  * [11] request or response
+        //  * [12] handlerName - we only care for the THREADFIX_HANDLER_NAME here
+        //  * [13] component which reject the request
+        //  * [14] value which rejects the request
+        //  * [16] error id (use this together with the timetamp to be unique)
+        //  * [17] free text field
+        //  * ... aditional stuff
 
-                try 
-                {
-		    // we are using an iterator here because this
-		    // is the interface of this CSV parser 
-		    // however, we always feed only one line into
-		    // this parser so it is ok to return from this
-		    // loop and never continue
-                    Iterable<CSVRecord> parser = CSVFormat.DEFAULT.parse(new StringReader(entry));
-                    for (CSVRecord record : parser) {
+        try {
+            // we are using an iterator here because this
+            // is the interface of this CSV parser 
+            // however, we always feed only one line into
+            // this parser so it is ok to return from this
+            // loop and never continue
+            Iterable<CSVRecord> parser = CSVFormat.DEFAULT.parse(new StringReader(entry));
+            for (CSVRecord record : parser) {
 
-                        // We access elements 0 .. 17 later, so this has to have at least 18 elements
-                        if (record.size() < 18)
-                        {
-                            log.error("can't parse logline: " +  entry);
-                            return null;
-                        }
-                        String csvTimestamp = record.get(0); 		// 20140131-172342
-                        String csvClientIP  = record.get(4);		// 10.17.23.41
-                        String csvRulesetMode = record.get(10);		// P or D
-                        String csvHandlerName = record.get(12);		// ThreadfixHandler
-                        String csvComponentName = record.get(13);	// protection_ruleset
-                        String csvComponentValue = record.get(14);	// threadfix:100042 or 100042
-                        String csvErrorId = record.get(16);		// 1234567
-                        String csvFreeText = record.get(17);		// free text which describe the action
+                // We access elements 0 .. 17 later, so this has to have at least 18 elements
+                if (record.size() < 18) {
+                    log.error("can't parse logline: " + entry);
+                    return null;
+                }
+                String csvTimestamp = record.get(0); 		// 20140131-172342
+                String csvClientIP = record.get(4);		// 10.17.23.41
+                String csvRulesetMode = record.get(10);		// P or D
+                String csvHandlerName = record.get(12);		// ThreadfixHandler
+                String csvComponentName = record.get(13);	// protection_ruleset
+                String csvComponentValue = record.get(14);	// threadfix:100042 or 100042
+                String csvErrorId = record.get(16);		// 1234567
+                String csvFreeText = record.get(17);		// free text which describe the action
 
-                        if( csvTimestamp == null || csvClientIP == null || csvHandlerName == null || csvRulesetMode == null ||
-			    csvComponentName == null || csvComponentValue == null || csvErrorId == null || csvFreeText == null)  {
+                if (csvTimestamp == null || csvClientIP == null || csvHandlerName == null || csvRulesetMode == null
+                        || csvComponentName == null || csvComponentValue == null || csvErrorId == null || csvFreeText == null) {
 
-                            log.error("can't parse logline: " + entry);
-                            return null;
-                        }
+                    log.error("can't parse logline: " + entry);
+                    return null;
+                }
 
-                        // we only care for THREADFIX_HANDLER_NAME here ... ignore all other stuff
-                        if (! csvHandlerName.equals(THREADFIX_HANDLER_NAME))
-                        {
-                            log.debug("ignore unknown handler: "+ csvHandlerName);
-                            return null;
-                        }
+                // we only care for THREADFIX_HANDLER_NAME here ... ignore all other stuff
+                if (!csvHandlerName.equals(THREADFIX_HANDLER_NAME)) {
+                    log.debug("ignore unknown handler: " + csvHandlerName);
+                    return null;
+                }
 
-			// while the error id act more or less as
-			// a unique id for rejected requests, this id
-			// is too short to be really unique over a
-			// long time. So we combine it here with the
-			// timestamp to get a better native id
-                        String nativeId = csvTimestamp + "-" + csvErrorId;
+                // while the error id act more or less as
+                // a unique id for rejected requests, this id
+                // is too short to be really unique over a
+                // long time. So we combine it here with the
+                // timestamp to get a better native id
+                String nativeId = csvTimestamp + "-" + csvErrorId;
 
-                        log.debug("native id: " + nativeId);
-                        
-                        if (securityEventDao.retrieveByNativeIdAndWafId(nativeId, wafId) != null)
-                            return null;
+                log.debug("native id: " + nativeId);
 
-                        String wafRuleId = null;
-                        if( csvComponentName.equals(THREADFIX_HANDLER_COMPONENT)) {
-                            // allow threadfix:123456 and 123456
-                            if (csvComponentValue.contains(":"))
-                            {
-                                wafRuleId = csvComponentValue.split(":",2)[1];
-                            }
-                            else
-                            {
-                                wafRuleId = csvComponentValue;
-                            }
-                        } else {
-                            log.debug("ignore unknown component: " + csvComponentName);
-                            return null;
-                        }
+                if (securityEventDao.retrieveByNativeIdAndWafId(nativeId, wafId) != null) {
+                    return null;
+                }
 
-                        log.debug("wafRuleId " + wafRuleId);
-
-                        WafRule rule = wafRuleDao.retrieveByWafAndNativeId(wafId, wafRuleId);
-                        if (rule == null) {
-                            log.debug("wafRule not found");
-                            return null;
-                        }
-
-                        Calendar calendar = parseDate(csvTimestamp);
-
-			if (calendar == null) {
-                            log.error("can't parse logline (timestamp): " + entry);
-                            return null;
-                        }
-                        
-                        SecurityEvent event = new SecurityEvent();
-                        
-                        event.setWafRule(rule);
-                        event.setImportTime(calendar);
-                        event.setLogText(csvFreeText);
-
-                        event.setAttackType("deny");
-			//if (csvRulesetMode == WAF_LOG_MODE_PROTECTION)
-			//{
-			//    event.setAttackType("deny");
-			//} else {
-			//    event.setAttackType("log"); 
-			//}
-                        event.setNativeId(nativeId);
-                        event.setAttackerIP(csvClientIP);
-
-                        
-                        return event;
+                String wafRuleId = null;
+                if (csvComponentName.equals(THREADFIX_HANDLER_COMPONENT)) {
+                    // allow threadfix:123456 and 123456
+                    if (csvComponentValue.contains(":")) {
+                        wafRuleId = csvComponentValue.split(":", 2)[1];
+                    } else {
+                        wafRuleId = csvComponentValue;
                     }
-                }
-                catch (IOException e)
-                {
+                } else {
+                    log.debug("ignore unknown component: " + csvComponentName);
                     return null;
                 }
-                return null;
 
-	}
-	
-	public static Calendar parseDate(String time) {
-		SimpleDateFormat formatter = new SimpleDateFormat(LOG_TIMESTAMP_FORMAT);
-		Date date = null;
+                log.debug("wafRuleId " + wafRuleId);
 
-		if (time == null)
-			return null;
-
-		try {
-			date = formatter.parse(time);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		
-		if (date == null) {
+                WafRule rule = wafRuleDao.retrieveByWafAndNativeId(wafId, wafRuleId);
+                if (rule == null) {
+                    log.debug("wafRule not found");
                     return null;
                 }
-		
-		Calendar calendar = Calendar.getInstance();
-		calendar.setTime(date);
-		
-		return calendar;
-	}
 
+                Calendar calendar = parseDate(csvTimestamp);
+
+                if (calendar == null) {
+                    log.error("can't parse logline (timestamp): " + entry);
+                    return null;
+                }
+
+                SecurityEvent event = new SecurityEvent();
+
+                event.setWafRule(rule);
+                event.setImportTime(calendar);
+                event.setLogText(csvFreeText);
+
+                event.setAttackType("deny");
+                //if (csvRulesetMode == WAF_LOG_MODE_PROTECTION)
+                //{
+                //    event.setAttackType("deny");
+                //} else {
+                //    event.setAttackType("log"); 
+                //}
+                event.setNativeId(nativeId);
+                event.setAttackerIP(csvClientIP);
+
+
+                return event;
+            }
+        } catch (IOException e) {
+            return null;
+        }
+        return null;
+
+    }
+
+    public static Calendar parseDate(String time) {
+        SimpleDateFormat formatter = new SimpleDateFormat(LOG_TIMESTAMP_FORMAT);
+        Date date = null;
+
+        if (time == null) {
+            return null;
+        }
+
+        try {
+            date = formatter.parse(time);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        if (date == null) {
+            return null;
+        }
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        return calendar;
+    }
 }
