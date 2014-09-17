eaf2d5f014549b908ffff07fc3cc7024ba9129f4
Mirko Dziadzka <mirko.dziadzka@riverbed.com>
2014-09-17 10:37:17 +0200

    remove unneeded files

--- threadfix-upgrade/hsql-upgrade/resources/add-steelapp-waf.sql
+++
@@ -1,4 +0,0 @@
--- add entries for Riverbed WAF (SteelApp Web App Firewall)
-INSERT INTO WAFTYPE (initialid, name) VALUES (100000, 'SteelApp Web App Firewall');
-INSERT INTO WAFRULEDIRECTIVE (directive, waftypeid) VALUES ('deny', (SELECT id FROM WAFTYPE WHERE name = 'SteelApp Web App Firewall'));
-
