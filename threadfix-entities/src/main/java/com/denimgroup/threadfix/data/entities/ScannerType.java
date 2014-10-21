c77d17e77aa8b1cb710a60fd5327a9621cce2ecd
Zignat Abdisubhan <zabdisubhan@denimgroup.com>
2014-10-21 15:56:19 -0500

    adding 3 arg constructor for ScannerType

--- threadfix-entities/src/main/java/com/denimgroup/threadfix/data/entities/ScannerType.java
+++ threadfix-entities/src/main/java/com/denimgroup/threadfix/data/entities/ScannerType.java
@@ -27,31 +27,31 @@ package com.denimgroup.threadfix.data.entities;
 import static com.denimgroup.threadfix.data.entities.ScannerDatabaseNames.*;
 
 public enum ScannerType {
-    ACUNETIX_WVS("acunetix", "Acunetix WVS", ACUNETIX_WVS_DB_NAME, ""),
-    APPSCAN_DYNAMIC("appscan", "IBM Rational AppScan", APPSCAN_DYNAMIC_DB_NAME, ""),
-    ARACHNI("arachni", "Arachni", ARACHNI_DB_NAME, ""),
-    BRAKEMAN("brakeman", "Brakeman", BRAKEMAN_DB_NAME, ""),
-    BURPSUITE("burp", "Burp Suite", BURPSUITE_DB_NAME, ""),
-    CAT_NET("catnet", "Microsoft CAT.NET", CAT_NET_DB_NAME, ""),
-    CENZIC_HAILSTORM("cenzic", "Cenzic Hailstorm", CENZIC_HAILSTORM_DB_NAME, ""),
-    CHECKMARX("checkmarx", "CheckMarx", CHECKMARX_DB_NAME, ""),
-    DEPENDENCY_CHECK("dependencycheck", "Dependency Check", DEPENDENCY_CHECK_DB_NAME, ""),
-    FINDBUGS("findbugs", "FindBugs", FINDBUGS_DB_NAME, ""),
-    FORTIFY("fortify", "Fortify SCA", FORTIFY_DB_NAME, ""),
-    NESSUS("nessus", "Nessus", NESSUS_DB_NAME, ""),
-    NTO_SPIDER("nto", "NTO Spider", NTO_SPIDER_DB_NAME, ""),
-    NETSPARKER("netsparker", "Mavituna Security Netsparker", NETSPARKER_DB_NAME, ""),
-    SKIPFISH("skipfish", "Skipfish", SKIPFISH_DB_NAME, ""),
-    W3AF("w3af", "w3af", W3AF_DB_NAME, ""),
-    WEBINSPECT("webinspect", "WebInspect", WEBINSPECT_DB_NAME, ""),
-    ZAPROXY("zap", "OWASP Zed Attack Proxy", ZAPROXY_DB_NAME, ""),
-    APPSCAN_SOURCE("appscansource", "IBM Rational AppScan Source Edition", APPSCAN_SOURCE_DB_NAME, ""),
-    APPSCAN_ENTERPRISE("appscanenterprise", "IBM Rational AppScan Enterprise", APPSCAN_ENTERPRISE_DB_NAME, ""),
-    QUALYSGUARD_WAS("qualysguard", "QualysGuard WAS", QUALYSGUARD_WAS_DB_NAME, ""),
+    ACUNETIX_WVS("acunetix", "Acunetix WVS", ACUNETIX_WVS_DB_NAME),
+    APPSCAN_DYNAMIC("appscan", "IBM Rational AppScan", APPSCAN_DYNAMIC_DB_NAME),
+    ARACHNI("arachni", "Arachni", ARACHNI_DB_NAME),
+    BRAKEMAN("brakeman", "Brakeman", BRAKEMAN_DB_NAME),
+    BURPSUITE("burp", "Burp Suite", BURPSUITE_DB_NAME),
+    CAT_NET("catnet", "Microsoft CAT.NET", CAT_NET_DB_NAME),
+    CENZIC_HAILSTORM("cenzic", "Cenzic Hailstorm", CENZIC_HAILSTORM_DB_NAME),
+    CHECKMARX("checkmarx", "CheckMarx", CHECKMARX_DB_NAME),
+    DEPENDENCY_CHECK("dependencycheck", "Dependency Check", DEPENDENCY_CHECK_DB_NAME),
+    FINDBUGS("findbugs", "FindBugs", FINDBUGS_DB_NAME),
+    FORTIFY("fortify", "Fortify SCA", FORTIFY_DB_NAME),
+    NESSUS("nessus", "Nessus", NESSUS_DB_NAME),
+    NTO_SPIDER("nto", "NTO Spider", NTO_SPIDER_DB_NAME),
+    NETSPARKER("netsparker", "Mavituna Security Netsparker", NETSPARKER_DB_NAME),
+    SKIPFISH("skipfish", "Skipfish", SKIPFISH_DB_NAME),
+    W3AF("w3af", "w3af", W3AF_DB_NAME),
+    WEBINSPECT("webinspect", "WebInspect", WEBINSPECT_DB_NAME),
+    ZAPROXY("zap", "OWASP Zed Attack Proxy", ZAPROXY_DB_NAME),
+    APPSCAN_SOURCE("appscansource", "IBM Rational AppScan Source Edition", APPSCAN_SOURCE_DB_NAME),
+    APPSCAN_ENTERPRISE("appscanenterprise", "IBM Rational AppScan Enterprise", APPSCAN_ENTERPRISE_DB_NAME),
+    QUALYSGUARD_WAS("qualysguard", "QualysGuard WAS", QUALYSGUARD_WAS_DB_NAME),
     SENTINEL("whitehat", "WhiteHat Sentinel", SENTINEL_DB_NAME, "https://source.whitehatsec.com/site_vuln_detail.html"),
-    SSVL("ssvl", "SSVL", SSVL_DB_NAME, ""),
-    VERACODE("veracode", "Veracode", VERACODE_DB_NAME, ""),
-    MANUAL("manual", "Manual", MANUAL_DB_NAME, "");
+    SSVL("ssvl", "SSVL", SSVL_DB_NAME),
+    VERACODE("veracode", "Veracode", VERACODE_DB_NAME),
+    MANUAL("manual", "Manual", MANUAL_DB_NAME);
 
 	private String fullName;
 	private String shortName;
@@ -79,6 +79,13 @@ public enum ScannerType {
         this.fullName = fullName;
         this.dbName = dbName;
         this.baseUrl = baseUrl;
+    }
+
+    private ScannerType(String shortName, String fullName, String dbName) {
+        this.shortName = shortName;
+        this.fullName = fullName;
+        this.dbName = dbName;
+        this.baseUrl = "";
     }
 	
 	public static ScannerType getScannerType(String keyword) {
