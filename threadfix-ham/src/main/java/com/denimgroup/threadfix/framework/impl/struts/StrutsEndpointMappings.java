bc1d9368c2a51cc7d9fec113dc0846d382c0fcfe
sgerick <sgerick@denimgroup.com>
2014-12-09 15:28:22 -0600

    Unit tests for Struts endpoints.

---
+++ threadfix-ham/src/main/java/com/denimgroup/threadfix/framework/impl/struts/StrutsEndpointMappings.java
@@ -0,0 +1,162 @@
+////////////////////////////////////////////////////////////////////////
+//
+//     Copyright (c) 2009-2014 Denim Group, Ltd.
+//
+//     The contents of this file are subject to the Mozilla Public License
+//     Version 2.0 (the "License"); you may not use this file except in
+//     compliance with the License. You may obtain a copy of the License at
+//     http://www.mozilla.org/MPL/
+//
+//     Software distributed under the License is distributed on an "AS IS"
+//     basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//     License for the specific language governing rights and limitations
+//     under the License.
+//
+//     The Original Code is ThreadFix.
+//
+//     The Initial Developer of the Original Code is Denim Group, Ltd.
+//     Portions created by Denim Group, Ltd. are Copyright (C)
+//     Denim Group, Ltd. All Rights Reserved.
+//
+//     Contributor(s): Denim Group, Ltd.
+//
+////////////////////////////////////////////////////////////////////////
+package com.denimgroup.threadfix.framework.impl.struts;
+
+import com.denimgroup.threadfix.data.interfaces.Endpoint;
+import com.denimgroup.threadfix.framework.engine.full.EndpointGenerator;
+import com.denimgroup.threadfix.framework.filefilter.FileExtensionFileFilter;
+import com.denimgroup.threadfix.framework.impl.model.ModelField;
+import com.denimgroup.threadfix.framework.impl.struts.model.StrutsAction;
+import com.denimgroup.threadfix.framework.impl.struts.model.StrutsPackage;
+import com.denimgroup.threadfix.framework.util.FilePathUtils;
+import com.denimgroup.threadfix.framework.util.java.EntityMappings;
+import com.denimgroup.threadfix.framework.util.java.EntityParser;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
+
+import javax.annotation.Nonnull;
+import java.io.File;
+import java.util.*;
+
+import static com.denimgroup.threadfix.CollectionUtils.list;
+
+public class StrutsEndpointMappings implements EndpointGenerator {
+
+    private final String STRUTS_CONFIG_NAME = "struts.xml";
+    private final String STRUTS_PROPERTIES_NAME = "struts.properties";
+
+    private File rootDirectory;
+    private Collection<File> javaFiles;
+    private List<StrutsPackage> strutsPackages;
+    private String strutsActionExtension;
+//    private Map<String, Set<StrutsEndpoint>> urlToControllerMethodsMap;
+    private List<Endpoint> endpoints;
+
+    public StrutsEndpointMappings(@Nonnull File rootDirectory) {
+        this.rootDirectory = rootDirectory;
+//        urlToControllerMethodsMap = new HashMap<>();
+        File strutsConfigFile = null;
+        File strutsPropertiesFile = null;
+
+        if (rootDirectory.exists()) {
+            javaFiles = FileUtils.listFiles(rootDirectory,
+                    new FileExtensionFileFilter("java"), TrueFileFilter.TRUE);
+        } else {
+            javaFiles = Collections.emptyList();
+        }
+
+        String[] configExtensions = {"xml", "properties"};
+        Collection configFiles = FileUtils.listFiles(rootDirectory, configExtensions, true);
+
+        for (Iterator iterator = configFiles.iterator(); iterator.hasNext();) {
+            File file = (File) iterator.next();
+            if (file.getName().equals(STRUTS_CONFIG_NAME))
+                strutsConfigFile = file;
+            if (file.getName().equals(STRUTS_PROPERTIES_NAME))
+                strutsPropertiesFile = file;
+            if (strutsConfigFile != null && strutsPropertiesFile != null)
+                break;
+        }
+
+        strutsActionExtension = StrutsPropertiesParser.getStrutsProperties(strutsPropertiesFile)
+                .getProperty("struts.action.extension","action");
+        if (strutsActionExtension == null)
+            strutsActionExtension = "";
+        strutsActionExtension = strutsActionExtension.trim();
+        if (strutsActionExtension.length() > 0
+                && !strutsActionExtension.startsWith(".")) {
+            strutsActionExtension = "." + strutsActionExtension;
+        }
+
+        strutsPackages = StrutsXmlParser.parse(strutsConfigFile);
+
+        generateMaps();
+
+    }
+
+    private void generateMaps() {
+        endpoints = list();
+        for (StrutsPackage strutsPackage : strutsPackages) {
+            String namespace = strutsPackage.getNamespace();
+            for (StrutsAction strutsAction : strutsPackage.getActions()) {
+                StringBuilder sbUrl = new StringBuilder(namespace);
+                String actionName = strutsAction.getName();
+
+                sbUrl.append("/");
+                sbUrl.append( actionName );
+                sbUrl.append( strutsActionExtension );
+
+                if (strutsAction.getActClass() == null)
+                    continue;
+
+                File actionFile = getJavaFileByName(strutsAction.getActClass());
+                EntityParser entityParser = EntityParser.parse(actionFile);
+                String filePath = FilePathUtils.getRelativePath(actionFile, rootDirectory);
+                String urlPath = sbUrl.toString();
+                Set<ModelField> fieldMappings = entityParser.getFieldMappings();
+                List<String> httpMethods = list();
+                List<String> parameters = list();
+                httpMethods.add( "POST" );
+                for (ModelField mf : fieldMappings) {
+                    parameters.add( mf.getParameterKey() );
+                }
+                endpoints.add( new StrutsEndpoint(filePath, urlPath, httpMethods, parameters) );
+                System.err.println();
+                System.err.println(filePath);
+                System.err.println(urlPath);
+                System.err.println(parameters);
+            }
+        }
+
+    }
+
+    private File getJavaFileByName(String fileName) {
+        fileName = fileName.replace('.', '/');
+        fileName = fileName.concat(".java");
+        for (File f : javaFiles) {
+            String filePath = f.getPath();
+            if (filePath.contains("\\"))
+                filePath = filePath.replace('\\','/');
+            if (filePath.endsWith(fileName))
+                return f;
+        }
+        return null;
+    }
+
+    @Nonnull
+    @Override
+    public List<Endpoint> generateEndpoints() {
+        return endpoints;
+    }
+
+    /**
+     * Returns an iterator over a set of elements of type T.
+     *
+     * @return an Iterator.
+     */
+    @Override
+    public Iterator<Endpoint> iterator() {
+        return generateEndpoints().iterator();
+    }
+}
---
+++ threadfix-ham/src/test/java/com/denimgroup/threadfix/framework/impl/struts/StrutsEndpointMappingsTests.java
@@ -0,0 +1,106 @@
+////////////////////////////////////////////////////////////////////////
+//
+//     Copyright (c) 2009-2014 Denim Group, Ltd.
+//
+//     The contents of this file are subject to the Mozilla Public License
+//     Version 2.0 (the "License"); you may not use this file except in
+//     compliance with the License. You may obtain a copy of the License at
+//     http://www.mozilla.org/MPL/
+//
+//     Software distributed under the License is distributed on an "AS IS"
+//     basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//     License for the specific language governing rights and limitations
+//     under the License.
+//
+//     The Original Code is ThreadFix.
+//
+//     The Initial Developer of the Original Code is Denim Group, Ltd.
+//     Portions created by Denim Group, Ltd. are Copyright (C)
+//     Denim Group, Ltd. All Rights Reserved.
+//
+//     Contributor(s): Denim Group, Ltd.
+//
+////////////////////////////////////////////////////////////////////////
+package com.denimgroup.threadfix.framework.impl.struts;
+
+import com.denimgroup.threadfix.data.enums.FrameworkType;
+import com.denimgroup.threadfix.data.interfaces.Endpoint;
+import com.denimgroup.threadfix.framework.TestConstants;
+import com.denimgroup.threadfix.framework.engine.cleaner.DefaultPathCleaner;
+import com.denimgroup.threadfix.framework.engine.full.EndpointDatabase;
+import com.denimgroup.threadfix.framework.engine.full.EndpointDatabaseFactory;
+import com.denimgroup.threadfix.framework.engine.full.EndpointQuery;
+import com.denimgroup.threadfix.framework.engine.full.EndpointQueryBuilder;
+import com.denimgroup.threadfix.framework.impl.spring.SpringControllerMappings;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+
+public class StrutsEndpointMappingsTests {
+	
+//	@Test
+//	public void printEndpoints() {
+//		File file = new File(TestConstants.PETCLINIC_SOURCE_LOCATION);
+//		SpringControllerMappings mappings = new SpringControllerMappings(file);
+//
+//		for (Endpoint endpoint: mappings.generateEndpoints()) {
+//			System.out.println(endpoint);
+//		}
+//	}
+	
+	@Test
+	public void testRoller() {
+        File rootFile = new File("C:/SourceCode/roller-weblogger-5.1.1-source/app/src");
+        StrutsEndpointMappings mappings = new StrutsEndpointMappings(rootFile);
+
+        EndpointDatabase database = EndpointDatabaseFactory.getDatabase(
+                mappings,
+                FrameworkType.STRUTS,
+                new DefaultPathCleaner("",""));
+
+        test(database, "/main/java/org/apache/roller/weblogger/ui/struts2/core/Register.java",
+                "/roller-ui/register!*.rol", "POST",
+                "servletRequest", "activationStatus", "bean", "activationCode", "authMethod");
+
+        test(database, "/main/java/org/apache/roller/weblogger/ui/struts2/admin/UserEdit.java",
+                "/roller-ui/admin/modifyUser!*.rol", "POST", "bean", "authMethod");
+
+        test(database, "/main/java/org/apache/roller/weblogger/ui/struts2/editor/MediaFileAdd.java",
+                "/roller-ui/authoring/overlay/mediaFileAdd!*.rol", "POST", "bean", "directoryName", "directory");
+
+        test(database, "/main/java/org/apache/roller/weblogger/ui/struts2/editor/MediaFileImageChooser.java",
+                "/roller-ui/authoring/overlay/mediaFileImageChooser!*.rol", "POST",
+                "currentDirectory", "directoryId", "directoryName");
+
+        test(database, "/main/java/org/apache/roller/weblogger/ui/struts2/editor/ThemeEdit.java",
+                "/roller-ui/authoring/themeEdit!*.rol", "POST", "themeId", "themeType", "selectedThemeId");
+
+        test(database, "/main/java/org/apache/roller/weblogger/ui/struts2/editor/Comments.java",
+                "/roller-ui/authoring/comments!*.rol", "POST",
+                "pager", "lastComment", "bulkDeleteCount", "bean", "queryEntry", "firstComment");
+
+    }
+
+    private void test(EndpointDatabase database, String fileName, String url, String method, String... parameters) {
+
+        EndpointQuery endpointQuery = EndpointQueryBuilder.start().setDynamicPath(url).generateQuery();
+
+        Endpoint bestMatch = database.findBestMatch(endpointQuery);
+
+        assert bestMatch != null : "No match found for url " + url;
+
+        assert bestMatch.getFilePath().equals(fileName) :
+                "Endpoint didn't match fileName " + fileName + ", got " + bestMatch.getFilePath() + " instead.";
+
+        assert bestMatch.getHttpMethods().contains(method) :
+                "Endpoint didn't have HTTP method " + method + ", had " + bestMatch.getHttpMethods();
+
+        for (String parameter : parameters) {
+            assert bestMatch.getParameters().contains(parameter) : "Parameters didn't contain " + parameter;
+        }
+
+    }
+
+}
