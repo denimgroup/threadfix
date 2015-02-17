c6ab3671d31d4728e49b8513e3537b0d78cffc7f
Jack <Jack@jacks-mac-mini.denimgroup.com>
2015-02-17 13:59:58 -0600

    Changed login url for tests to maven system variable

--- threadfix-main/src/test/java/com/denimgroup/threadfix/selenium/pages/LoginPage.java
+++ threadfix-main/src/test/java/com/denimgroup/threadfix/selenium/pages/LoginPage.java
@@ -33,9 +33,7 @@ import java.net.URL;
 
 public class LoginPage extends BasePage {
 
-	public static String url = "http://localhost:8080/threadfix/login.jsp";
-    //public static String url = "https://localhost:8443/threadfix/";
-    //public static String url = "http://jenkins-slave5:8080/threadfix/";
+	public static String url = System.getProperty("LOGIN_URL");
 
 	public LoginPage(WebDriver webdriver) {
 		super(webdriver);
