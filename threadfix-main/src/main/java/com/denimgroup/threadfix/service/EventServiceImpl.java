76f80683823861f38ec56ad2163222526ab28ff3
Daryl Shannon <dshannon@denimgroup.com>
2015-04-20 17:20:47 -0500

    Added DAO and Service classes for basic access to new Event entities.

---
+++ threadfix-main/src/main/java/com/denimgroup/threadfix/service/EventServiceImpl.java
@@ -0,0 +1,72 @@
+////////////////////////////////////////////////////////////////////////
+//
+//     Copyright (c) 2009-2015 Denim Group, Ltd.
+//
+//     The contents of this file are subject to the Mozilla Public License
+//     Version 2.0 (the "License"); you may not use this file except in
+//     compliance with the License. You may obtain a copy of the License at
+//     http://www.mozilla.org/MPL/
+//
+//     Software distributed under the License is distributed on an "AS IS"
+//     basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//     License for the specific language governing rights and limitations
+//     under the License.
+//
+//     The Original Code is ThreadFix.
+//
+//     The Initial Developer of the Original Code is Denim Group, Ltd.
+//     Portions created by Denim Group, Ltd. are Copyright (C)
+//     Denim Group, Ltd. All Rights Reserved.
+//
+//     Contributor(s): Denim Group, Ltd.
+//
+////////////////////////////////////////////////////////////////////////
+
+package com.denimgroup.threadfix.service;
+
+import com.denimgroup.threadfix.data.dao.UserDao;
+import com.denimgroup.threadfix.data.dao.EventDao;
+import com.denimgroup.threadfix.data.dao.VulnerabilityDao;
+import com.denimgroup.threadfix.data.entities.Event;
+import com.denimgroup.threadfix.logging.SanitizedLogger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Nullable;
+import java.util.Date;
+
+@Service
+@Transactional
+public class EventServiceImpl implements EventService {
+
+	private final SanitizedLogger log = new SanitizedLogger(EventService.class);
+
+    @Autowired
+    private EventDao eventDao;
+    @Autowired
+    private VulnerabilityDao vulnerabilityDao;
+    @Autowired
+	private UserDao userDao;
+    @Autowired(required = false)
+    @Nullable
+    private PermissionService permissionService;
+
+	@Override
+	public String createEvent() {
+		Event newEvent = new Event();
+		eventDao.saveOrUpdate(newEvent);
+
+		return VALID;
+	}
+
+	@Override
+    public void saveEvent(Event event) {
+		eventDao.saveOrUpdate(event);
+    }
+
+    @Override
+    public Event loadEventById(int eventId) {
+        return eventDao.retrieveById(eventId);
+    }
+}
