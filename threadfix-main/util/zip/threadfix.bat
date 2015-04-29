f7dea168016efdda75bbee5cee9154c313bbeee5
RTimmonsDG <rtimmons@denimgroup.com>
2015-04-29 14:48:02 -0500

    Updates batch file to reflect Java 8 upgrade.

--- threadfix-main/util/zip/threadfix.bat
+++ threadfix-main/util/zip/threadfix.bat
@@ -15,9 +15,9 @@ set PWD=%cd%
 set CATALINA_HOME=%PWD%\tomcat
 set CATALINA_OPTS=-Xms512m -Xmx1536m -XX:PermSize=256m -XX:MaxPermSize=256m
 if DEFINED JAVA_HOME (
-	"%JAVA_HOME%"\bin\java -version:1.7 -version > nul 2>&1
-	if NOT %ERRORLEVEL% == 0 (
-		echo Local JAVA_HOME is not Java 7, switching to threadfix JAVA_HOME
+	"%JAVA_HOME%\bin\java" -version:1.8 -version > nul 2>&1
+	if NOT ERRORLEVEL == 0 (
+		echo Local JAVA_HOME is not Java 8, switching to threadfix JAVA_HOME
 		set JAVA_HOME=%PWD%\java
 	)
 ) else (
@@ -38,7 +38,7 @@ echo
 
 ping -n 10 127.0.0.1 > NUL:
 
-START "" "https://localhost:8443/threadfix"
+START "" "http://localhost:8080/threadfix/login.jsp"
 
 goto END
 
