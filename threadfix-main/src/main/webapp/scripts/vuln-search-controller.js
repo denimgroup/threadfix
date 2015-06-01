647744d315f2b94df0ffa02ec59657d6eb3467a5
Sinh Tran <stran@denimgroup.com>
2015-06-01 14:12:52 -0500

    DGTF-1225 Improve Tag UI by sorting tags

--- threadfix-main/src/main/webapp/scripts/vuln-search-controller.js
+++ threadfix-main/src/main/webapp/scripts/vuln-search-controller.js
@@ -52,6 +52,8 @@ module.controller('VulnSearchController', function($scope, $rootScope, $window, 
                         $scope.teams = data.object.teams;
                         $scope.tags = data.object.tags;
                         $scope.commentTags = data.object.commentTags;
+                        $scope.tags.sort(nameCompare);
+                        $scope.commentTags.sort(nameCompare);
                         $scope.scanners = data.object.scanners;
                         $scope.genericVulnerabilities = data.object.vulnTypes;
                         $scope.searchApplications = data.object.applications;
@@ -160,5 +162,9 @@ module.controller('VulnSearchController', function($scope, $rootScope, $window, 
         $scope.parameters = angular.copy(parameters);
         $scope.refresh();
     });
+
+    var nameCompare = function(a,b) {
+        return a.name.localeCompare(b.name);
+    };
 
 });
