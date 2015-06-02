e92b5e8f8d641d7af6258480929e7c0d55bb3c97
Srinithya <skakani@denimgroup.com>
2015-06-02 12:50:49 -0500

    Implemented methods for APP SCAN login and logout

--- threadfix-importers/src/main/java/com/denimgroup/threadfix/importer/impl/remoteprovider/AppScanEnterpriseRemoteProvider.java
+++ threadfix-importers/src/main/java/com/denimgroup/threadfix/importer/impl/remoteprovider/AppScanEnterpriseRemoteProvider.java
@@ -4,7 +4,11 @@ import com.denimgroup.threadfix.annotations.RemoteProvider;
 import com.denimgroup.threadfix.data.entities.RemoteProviderApplication;
 import com.denimgroup.threadfix.data.entities.Scan;
 import com.denimgroup.threadfix.data.entities.ScannerType;
+import com.denimgroup.threadfix.exception.RestIOException;
+import com.denimgroup.threadfix.importer.impl.remoteprovider.utils.HttpResponse;
 import com.denimgroup.threadfix.importer.impl.remoteprovider.utils.RemoteProviderHttpUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.List;
 
@@ -17,7 +21,15 @@ import static com.denimgroup.threadfix.importer.impl.remoteprovider.utils.Remote
 public class AppScanEnterpriseRemoteProvider extends AbstractRemoteProvider{
     public static final String
                 USERNAME = "Username",
-                PASSWORD = "Password";
+                PASSWORD = "Password",
+                URL = "URL",
+                BASE_URL = "ase/api/",
+                FEATURE_KEY = "AppScanEnterpriseUser",
+                LOGIN_SERVICE = "login",
+                LOGOUT_SERVICE = "logout",
+                APP_SERVICE = "applications";
+
+    public String sessionId = null;
 
     public AppScanEnterpriseRemoteProvider() {
       super(ScannerType.APPSCAN_ENTERPRISE);
@@ -37,6 +49,35 @@ public class AppScanEnterpriseRemoteProvider extends AbstractRemoteProvider{
         return null;
     }
 
+    private void loginToAppScanEnterprise(){
+        assert remoteProviderType != null : "Remote Provider Type is null, Please set it before trying to log in";
 
+        HttpResponse response = httpUtils.postUrl(getAuthenticationFieldValue(URL) + BASE_URL + LOGIN_SERVICE, new String[]{"featureKey"}, new String[]{"AppScanEnterpriseUser"}, getAuthenticationFieldValue(USERNAME), getAuthenticationFieldValue(PASSWORD));
+        if(response.isValid()){
+            JSONObject jsonObject = new JSONObject(response);
+            try{
+                sessionId = jsonObject.get("sessionId").toString();
+            }catch(JSONException e){
+                throw new RestIOException(e, "Invalid response received. May not be JSON");
+            }
+        }else{
+        String body = response.getBodyAsString();
+        log.info("Rest response from App Scan Enterprise Login Service:" + body);
+        throw new RestIOException("Invalid response with following status. Please Check logs for more details", response.getStatus());
+        }
+    }
 
+    private void logoutFromAppScanEnterprise(){
+        assert remoteProviderType != null : "Remote Provider Type is null, You shouldn't be here with out Remote provider. Check why this happened";
+
+        HttpResponse response = httpUtils.getUrl(getAuthenticationFieldValue(URL) + BASE_URL + LOGOUT_SERVICE);
+        if(response.isValid()){
+            log.info("Successfully logged out from APP SCAN ENTERPRISE");
+        }else {
+            String body = response.getBodyAsString();
+            log.info("Rest response from App Scan Enterprise Logout Service:" + body);
+            throw new RestIOException("Invalid response with following status. Please Check logs for more details", response.getStatus());
+        }
+
+    }
 }
