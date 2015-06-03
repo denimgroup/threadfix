1f52196a1f89299980a0268c91fa86979f98fa1a
jfessler <jfessler@denimgroup.com>
2015-06-03 13:57:52 -0500

    Login works

--- threadfix-importers/src/main/java/com/denimgroup/threadfix/importer/impl/remoteprovider/AppScanEnterpriseRemoteProvider.java
+++ threadfix-importers/src/main/java/com/denimgroup/threadfix/importer/impl/remoteprovider/AppScanEnterpriseRemoteProvider.java
@@ -5,6 +5,7 @@ import com.denimgroup.threadfix.data.entities.RemoteProviderApplication;
 import com.denimgroup.threadfix.data.entities.Scan;
 import com.denimgroup.threadfix.data.entities.ScannerType;
 import com.denimgroup.threadfix.exception.RestIOException;
+import com.denimgroup.threadfix.importer.impl.remoteprovider.utils.DefaultRequestConfigurer;
 import com.denimgroup.threadfix.importer.impl.remoteprovider.utils.HttpResponse;
 import com.denimgroup.threadfix.importer.impl.remoteprovider.utils.RemoteProviderHttpUtils;
 import com.denimgroup.threadfix.importer.impl.remoteprovider.utils.RequestConfigurer;
@@ -16,6 +17,7 @@ import java.util.List;
 
 import static com.denimgroup.threadfix.CollectionUtils.list;
 import static com.denimgroup.threadfix.importer.impl.remoteprovider.utils.RemoteProviderHttpUtilsImpl.getImpl;
+import static com.denimgroup.threadfix.importer.util.JsonUtils.getJSONObject;
 
 /**
  * Created by skakani on 5/26/2015.
@@ -26,12 +28,13 @@ public class AppScanEnterpriseRemoteProvider extends AbstractRemoteProvider{
                 USERNAME = "Username",
                 PASSWORD = "Password",
                 URL = "URL",
-                BASE_URL = "ase/api/",
+                BASE_URL = "/ase/api",
                 FEATURE_KEY = "AppScanEnterpriseUser",
-                LOGIN_SERVICE = "login",
-                LOGOUT_SERVICE = "logout",
-                APP_SERVICE = "applications",
-                SCAN_SERVICE = "issues";
+                LOGIN_SERVICE = "/login",
+                LOGOUT_SERVICE = "/logout",
+                APP_SERVICE = "/applications",
+                SCAN_SERVICE = "/issues",
+                LOGIN_JSON_FORMAT = "{ \"userId\": \"%s\", \"password\": \"%s\", \"featureKey\": \"AppScanEnterpriseUser\"}";
 
     public AppScanEnterpriseRemoteProvider() {
       super(ScannerType.APPSCAN_ENTERPRISE);
@@ -51,16 +54,33 @@ public class AppScanEnterpriseRemoteProvider extends AbstractRemoteProvider{
 
         List<Scan> scans = list();
 
-        try{
+//        try{
             String sessionId = loginToAppScanEnterprise();
+//
+//            String url = getAuthenticationFieldValue(URL) + BASE_URL + SCAN_SERVICE + "?Application Name=" + remoteProviderApplication.getNativeName();
+//
+//            HttpResponse response = httpUtils.getUrlWithConfigurer(url, getRequestConfigurer(sessionId));
+//
+//            if(response.isValid()){
+//                JSONObject json = getJSONObject(response.getBodyAsString());
+//
+//        JSONObject json = getJSONObject(TESTDATA);
+//
+//        List<Finding> findings = list();
+//
+//        for(JSONObject object : toJSONObjectIterable(json.getString("data"))){
 
-            String url = getAuthenticationFieldValue(URL) + BASE_URL + SCAN_SERVICE + "?Application Name=" + remoteProviderApplication.getNativeName();
+//        }
 
-            HttpResponse response = httpUtils.getUrlWithConfigurer(url, getRequestConfigurer(sessionId));
-
-        }catch (RestIOException e){
-            log.info("Error while retrieving scans from App Scan Enterprise");
-        }
+//            }else{
+//                log.warn("Received a bad response from App Scan Enterprise server, returning null");
+//                return null;
+//            }
+//
+//            logoutFromAppScanEnterprise();
+//        }catch (RestIOException e){
+//            log.info("Error while retrieving scans from App Scan Enterprise");
+//        }
 
         return scans;
     }
@@ -68,9 +88,11 @@ public class AppScanEnterpriseRemoteProvider extends AbstractRemoteProvider{
     private String loginToAppScanEnterprise(){
         assert remoteProviderType != null : "Remote Provider Type is null, Please set it before trying to log in";
 
-        HttpResponse response = httpUtils.postUrl(getAuthenticationFieldValue(URL) + BASE_URL + LOGIN_SERVICE, new String[]{"featureKey"}, new String[]{"AppScanEnterpriseUser"}, getAuthenticationFieldValue(USERNAME), getAuthenticationFieldValue(PASSWORD));
+        HttpResponse response = httpUtils.postUrlWithConfigurer(getAuthenticationFieldValue(URL) + BASE_URL + LOGIN_SERVICE, getLoginRequestConfigurer());
         if(response.isValid()){
-            JSONObject jsonObject = new JSONObject(response);
+            String responseBody = response.getBodyAsString();
+
+            JSONObject jsonObject = getJSONObject(responseBody);
             try{
                 return jsonObject.get("sessionId").toString();
             }catch(JSONException e){
@@ -106,5 +128,14 @@ public class AppScanEnterpriseRemoteProvider extends AbstractRemoteProvider{
                 method.setRequestHeader("asc_xsrf_token", sessionId);
             }
         };
+    }
+
+    private RequestConfigurer getLoginRequestConfigurer(){
+
+        String loginJSON = String.format(LOGIN_JSON_FORMAT, getAuthenticationFieldValue(USERNAME), getAuthenticationFieldValue(PASSWORD));
+
+        return new DefaultRequestConfigurer()
+                .withContentType("application/json")
+                .withRequestBody(loginJSON, "application/json");
     }
 }
