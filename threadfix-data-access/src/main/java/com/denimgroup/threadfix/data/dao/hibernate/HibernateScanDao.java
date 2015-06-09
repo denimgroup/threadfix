77275db32ca1d08e24b0e3e5b761bcecc6d68da9
Mac Collins <mcollins@denimgroup.com>
2015-06-09 12:58:17 -0500

    DGTF-1527 fixes remote provider deletion error.

--- threadfix-data-access/src/main/java/com/denimgroup/threadfix/data/dao/hibernate/HibernateScanDao.java
+++ threadfix-data-access/src/main/java/com/denimgroup/threadfix/data/dao/hibernate/HibernateScanDao.java
@@ -309,7 +309,7 @@ public class HibernateScanDao
 			}
 
 		}
-		
+
 		findings = null;
 
 		if (surfaceLocations != null) {
@@ -317,7 +317,7 @@ public class HibernateScanDao
 				sessionFactory.getCurrentSession().delete(surfaceLocation);
 			}
 		}
-		
+
 		sessionFactory.getCurrentSession().save(new DeletedScan(scan));
 		sessionFactory.getCurrentSession().delete(scan);
 	}
