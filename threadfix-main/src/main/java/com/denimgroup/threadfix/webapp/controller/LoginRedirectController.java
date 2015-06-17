c70fdb372cc33d4c0838e96b0b3256cd89e93bc9
Mac Collins <mcollins@denimgroup.com>
2015-06-17 13:08:08 -0500

    DGTF-1534 fixes issues related to pages not being found.

--- threadfix-main/src/main/java/com/denimgroup/threadfix/webapp/controller/LoginRedirectController.java
+++ threadfix-main/src/main/java/com/denimgroup/threadfix/webapp/controller/LoginRedirectController.java
@@ -28,10 +28,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login.jsp")
 public class LoginRedirectController {
 
-	@RequestMapping(";jsessionid={id}")
+	@RequestMapping("/login.jsp;jsessionid={id}")
 	public String processLinkDelete() {
 		return "redirect:/login.jsp";
 	}
