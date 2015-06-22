25c4b9f8705c8160cc40bcc8c00ebfee852876f7
d-maldonado <dmaldonado@denimgroup.com>
2015-06-22 16:27:50 -0500

    dgtf-1551 better error message when a user tries to login and no users are present in the DBS
    Conflicts:
    	threadfix-main/src/main/java/com/denimgroup/threadfix/service/UserServiceImpl.java

--- threadfix-main/src/main/java/com/denimgroup/threadfix/service/UserServiceImpl.java
+++ threadfix-main/src/main/java/com/denimgroup/threadfix/service/UserServiceImpl.java
@@ -417,6 +417,7 @@ public class UserServiceImpl implements UserService {
 	}
 
 	@Override
+	@Transactional(readOnly = true)
 	public Long countUsers(String searchString) {
 		return userDao.countUsers(searchString);
 	}
