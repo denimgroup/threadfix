9e429b09c9bbc0dbb92fc969d21c8cfbc5282dbf
David <dharrison@denimgroup.com>
2015-08-05 08:59:29 -0500

    Added a potential runtime fix to Group tests

--- threadfix-main/src/test/java/com/denimgroup/threadfix/selenium/pages/GroupIndexPage.java
+++ threadfix-main/src/test/java/com/denimgroup/threadfix/selenium/pages/GroupIndexPage.java
@@ -69,6 +69,7 @@ public class GroupIndexPage extends BasePage {
 
     public GroupIndexPage clickSaveGroup() {
         driver.findElementByCssSelector("button#submit:not(.disabled)").click();
+        waitForClickableElement(driver.findElementById("newGroupModalLink"));
         return new GroupIndexPage(driver);
     }
 
