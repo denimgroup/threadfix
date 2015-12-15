03a25a27526cd2673bcca93cc2d7477871e6d738
David <dharrison@denimgroup.com>
2015-12-15 10:28:46 -0600

    Modifies IDs in the reports to be more flexible with custom names for tests

--- threadfix-main/src/main/webapp/scripts/report/directives/d3-dashboards.js
+++ threadfix-main/src/main/webapp/scripts/report/directives/d3-dashboards.js
@@ -346,7 +346,7 @@ function barGraphData(d3, data, color, isLeftReport, label, reportConstants, tex
                 appId: (label && label.appId) ? label.appId : d.appId,
                 appName: d.appName,
                 severity: (topVulnsReport) ? undefined : key,
-                graphId: d.teamName + d.appName + ((topVulnsReport)? "CWE" + d.displayId : tip) + "Bar",
+                graphId: "bar" + d.teamName + d.appName + ((topVulnsReport)? "CWE" + d.displayId : tip),
                 genericSeverities: d.genericSeverities
             };
         });
