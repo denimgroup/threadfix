691f2b313d07f20d4868a115f5bd1be53788de76
Sinh Tran <stran@denimgroup.com>
2015-12-22 14:27:17 -0600

    create entity storage

---
+++ threadfix-entities/src/main/java/com/denimgroup/threadfix/data/entities/Confidence.java
@@ -0,0 +1,74 @@
+////////////////////////////////////////////////////////////////////////
+//
+//     Copyright (c) 2009-2015 Denim Group, Ltd.
+//
+//     The contents of this file are subject to the Mozilla Public License
+//     Version 2.0 (the "License"); you may not use this file except in
+//     compliance with the License. You may obtain a copy of the License at
+//     http://www.mozilla.org/MPL/
+//
+//     Software distributed under the License is distributed on an "AS IS"
+//     basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+//     License for the specific language governing rights and limitations
+//     under the License.
+//
+//     The Original Code is ThreadFix.
+//
+//     The Initial Developer of the Original Code is Denim Group, Ltd.
+//     Portions created by Denim Group, Ltd. are Copyright (C)
+//     Denim Group, Ltd. All Rights Reserved.
+//
+//     Contributor(s): Denim Group, Ltd.
+//
+////////////////////////////////////////////////////////////////////////
+package com.denimgroup.threadfix.data.entities;
+
+import com.denimgroup.threadfix.views.AllViews;
+import com.fasterxml.jackson.annotation.JsonView;
+
+import javax.persistence.*;
+import java.util.List;
+
+import static com.denimgroup.threadfix.CollectionUtils.list;
+
+
+@Entity
+@Table(name = "Confidence")
+public class Confidence extends AuditableEntity {
+
+	private SharedVulnerability sharedVulnerability;
+	private Double frontScore;
+	private Double backScore;
+
+
+	@OneToOne
+	@JoinColumn(name = "sharedVulnerability_Id")
+	@JsonView({ AllViews.VulnerabilityDetail.class })
+	public SharedVulnerability getSharedVulnerability() {
+		return sharedVulnerability;
+	}
+
+	public void setSharedVulnerability(SharedVulnerability sharedVulnerability) {
+		this.sharedVulnerability = sharedVulnerability;
+	}
+
+	@Column
+	@JsonView({ AllViews.VulnerabilityDetail.class })
+	public Double getFrontScore() {
+		return frontScore;
+	}
+
+	public void setFrontScore(Double frontScore) {
+		this.frontScore = frontScore;
+	}
+
+	@Column
+	@JsonView({ AllViews.VulnerabilityDetail.class })
+	public Double getBackScore() {
+		return backScore;
+	}
+
+	public void setBackScore(Double backScore) {
+		this.backScore = backScore;
+	}
+}
--- threadfix-service-interfaces/src/main/java/com/denimgroup/threadfix/service/SharedComponentService.java
+++ threadfix-service-interfaces/src/main/java/com/denimgroup/threadfix/service/SharedComponentService.java
@@ -35,4 +35,6 @@ public interface SharedComponentService {
 
 	List<Vulnerability> retrieveSharedVulns(Vulnerability vulnerability);
 
+	void updateAllSharedVulns();
+
 }
