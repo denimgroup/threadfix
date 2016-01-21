66f413706fd0dafe1fedc9e4303c06293327f2f2
David <dharrison@denimgroup.com>
2016-01-21 15:06:19 -0600

    Reworks IDs for vulnerability trees to be unique with pivots

--- threadfix-main/src/main/webapp/scripts/filters.js
+++ threadfix-main/src/main/webapp/scripts/filters.js
@@ -44,3 +44,23 @@ filtersModule.filter('removeEmailDomain', function() {
     }
 });
 
+filtersModule.filter('pivotForID', function() {
+    var akaRegex = /.*\(aka (.*)\)/;
+    var parensRegex = /.*\('(.*)'\)/;
+    return function(input) {
+        var test1 = akaRegex.exec(input);
+
+        if (test1) {
+            return test1[1].replace(/\W/g, '');
+        }
+
+        var test2 = parensRegex.exec(input);
+
+        if (test2) {
+            return test2[1].replace(/\W/g, '');
+        }
+
+        return input.replace(/\W/g, '');
+    }
+});
+
