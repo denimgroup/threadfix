702692b79560b336ccb08169f6d1572d9bcb889e
Dan Cornell <Dan@Dans-Laptop.local>
2018-09-21 16:04:25 -0500

    Moar deletes of files that have been moved

--- create-scanner-jar.sh
+++
@@ -1,5 +0,0 @@
-cd threadfix-main/target/classes
-jar cvf scanners.jar com/denimgroup/threadfix/plugin/scanner/service/** mappings/
-cd ../../
-cp target/classes/scanners.jar src/main/resources
-cd ..
--- ssvl-converter/src/main/java/com/denimgroup/threadfix/csv2ssl/util/Defaults.java
+++
@@ -1,9 +0,0 @@
-package com.denimgroup.threadfix.csv2ssl.util;
-
-/**
- * Created by mcollins on 12/10/2014.
- */
-public class Defaults {
-
-    public static boolean ALLOW_FILE_HEADERS = "true".equals(Strings.ALLOW_FILE_HEADERS);
-}
